f47c041ecfa533389ce38db0f9f8bc47b2d76c7d
sumant1512 <sumantmishra511@gmail.com>
2024-03-31 14:10:18 +0530

    shade card sample added with large modal

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -8,8 +8,21 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ProductComponent implements OnInit {
   shadeList = [
-    { colorCode: 'L123', colorName: 'Red', hexCode: '#d34d29' },
-    { colorCode: 'L124', colorName: 'Orange', hexCode: '#eaa245' },
+    {
+      colorCode: '8108',
+      colorName: 'Button Rose',
+      hexCode: 'rgb(242, 228, 223)',
+    },
+    {
+      colorCode: '5182',
+      colorName: 'Lucid Dream',
+      hexCode: '#E7D3D3',
+    },
+    {
+      colorCode: '9607',
+      colorName: 'Purple Galaxy-N',
+      hexCode: 'rgb(101, 71, 99)',
+    },
   ];
   shadeForm = new FormGroup({
     color: new FormControl(''),
