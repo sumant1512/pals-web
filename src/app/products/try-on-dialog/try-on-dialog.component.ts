5394a23b1994e80e5c01bd6a715700fb08678bd9
sumant1512 <sumantmishra511@gmail.com>
2024-04-06 23:07:52 +0530

    canvas added

--- src/app/products/try-on-dialog/try-on-dialog.component.ts
+++ src/app/products/try-on-dialog/try-on-dialog.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { fromEvent, pairwise, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-try-on-dialog',
   templateUrl: './try-on-dialog.component.html',
-  styleUrls: ['./try-on-dialog.component.scss']
+  styleUrls: ['./try-on-dialog.component.scss'],
 })
-export class TryOnDialogComponent {
+export class TryOnDialogComponent implements AfterViewInit {
+  @ViewChild('canvas') public canvas!: ElementRef;
 
+  @Input() public width = 400;
+  @Input() public height = 400;
+
+  private cx!: CanvasRenderingContext2D;
+
+  public ngAfterViewInit() {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.cx = canvasEl.getContext('2d') as any;
+
+    canvasEl.width = this.width;
+    canvasEl.height = this.height;
+
+    this.cx.lineWidth = 3;
+    this.cx.lineCap = 'round';
+    this.cx.strokeStyle = '#000';
+
+    this.captureEvents(canvasEl);
+  }
+
+  clearCanvas(): void {
+    this.cx.clearRect(0, 0, this.width, this.height);
+  }
+
+  private captureEvents(canvasEl: HTMLCanvasElement) {
+    fromEvent(canvasEl, 'mousedown')
+      .pipe(
+        switchMap((e) => {
+          return fromEvent(canvasEl, 'mousemove').pipe(
+            takeUntil(fromEvent(canvasEl, 'mouseup')),
+            takeUntil(fromEvent(canvasEl, 'mouseleave')),
+            pairwise() /* Return the previous and last values as array */
+          );
+        })
+      )
+      .subscribe(([mouseEvent, mouseEvent1]) => {
+        const rect = canvasEl.getBoundingClientRect();
+
+        const prevPos = {
+          x: (mouseEvent as MouseEvent).clientX - rect.left,
+          y: (mouseEvent as MouseEvent).clientY - rect.top,
+        };
+
+        const currentPos = {
+          x: (mouseEvent1 as MouseEvent).clientX - rect.left,
+          y: (mouseEvent1 as MouseEvent).clientY - rect.top,
+        };
+
+        this.drawOnCanvas(prevPos, currentPos);
+      });
+  }
+
+  private drawOnCanvas(
+    prevPos: { x: number; y: number },
+    currentPos: { x: number; y: number }
+  ) {
+    if (!this.cx) {
+      return;
+    }
+
+    this.cx.beginPath();
+
+    if (prevPos) {
+      this.cx.moveTo(prevPos.x, prevPos.y); // from
+      this.cx.lineTo(currentPos.x, currentPos.y);
+      this.cx.stroke();
+    }
+  }
 }
