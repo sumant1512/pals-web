10c0c529c0113c91534c80ffe12c27722e630d92
sumant1512 <sumantmishra511@gmail.com>
2025-07-09 23:27:13 +0530

    get in touch button updated

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent {
+  constructor(private router: Router) {}
 
+  navigateToProductsPage(): void {
+    this.router.navigate(['products']);
+  }
 }
