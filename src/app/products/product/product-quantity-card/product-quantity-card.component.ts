7564dbd708bb5869fdbfc42c61533d60954a6029
sumant1512 <sumantmishra511@gmail.com>
2025-07-14 00:28:25 +0530

    mobile card added

--- src/app/products/product/product-quantity-card/product-quantity-card.component.ts
+++ src/app/products/product/product-quantity-card/product-quantity-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-quantity-card',
   templateUrl: './product-quantity-card.component.html',
-  styleUrls: ['./product-quantity-card.component.scss']
+  styleUrls: ['./product-quantity-card.component.scss'],
 })
 export class ProductQuantityCardComponent {
-
+  @Input() packSize!: string;
+  @Input() price!: string;
 }
