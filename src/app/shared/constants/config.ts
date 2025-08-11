b2d141e6b6d31c5816e6d75980d90f9c6604501e
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2025-08-12 00:30:55 +0530

    Update backend path

--- src/app/shared/constants/config.ts
+++ src/app/shared/constants/config.ts
@@ -1,4 +1,4 @@
-const BE_PROD_PATH = 'http://localhost:8080';
+const BE_PROD_PATH = 'https://pals-app-b3zs.onrender.com';
 const BE_LOCAL_PATH = 'http://localhost:8080/product';
 const BE_JSON_PATH = BE_PROD_PATH;
 
