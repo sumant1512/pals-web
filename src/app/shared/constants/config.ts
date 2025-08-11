c56b9921eedf223b93aff010d7f872d36db052d8
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2025-08-12 00:33:41 +0530

    Updated prod path

--- src/app/shared/constants/config.ts
+++ src/app/shared/constants/config.ts
@@ -3,4 +3,4 @@ const BE_LOCAL_PATH = 'http://localhost:8080/product';
 const BE_JSON_PATH = BE_PROD_PATH;
 
 export const IS_JSON = true;
-export const ACTIVE_BE = BE_JSON_PATH;
+export const ACTIVE_BE = BE_PROD_PATH;
