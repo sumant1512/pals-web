d7acff1cccbc0b6a12f3924e36bc1b73dc91e88a
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2025-08-14 00:36:33 +0530

    updated to real database

--- src/app/shared/constants/config.ts
+++ src/app/shared/constants/config.ts
@@ -2,5 +2,5 @@ const BE_PROD_PATH = 'https://pals-app-b3zs.onrender.com';
 const BE_LOCAL_PATH = 'http://localhost:8080/product';
 const BE_JSON_PATH = BE_PROD_PATH;
 
-export const IS_JSON = true;
+export const IS_JSON = false;
 export const ACTIVE_BE = BE_PROD_PATH;
