7a352d3289830c4a782a80440616c2dc49f11107
sumant1512 <sumantmishra511@gmail.com>
2025-11-16 00:14:53 +0530

    config changed

--- src/app/shared/constants/config.ts
+++ src/app/shared/constants/config.ts
@@ -1,4 +1,4 @@
 const BE_PROD_PATH = 'https://pals-app-b3zs.onrender.com';
 const BE_LOCAL_PATH = 'http://localhost:8080';
 
-export const ACTIVE_BE = BE_LOCAL_PATH;
+export const ACTIVE_BE = BE_PROD_PATH;
