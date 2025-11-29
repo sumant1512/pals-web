a907337d9cf1a85f247d958cc89482204efb572e
sumant1512 <sumantmishra511@gmail.com>
2025-11-29 14:36:46 +0530

    pointing back to dev be

--- src/app/shared/constants/config.ts
+++ src/app/shared/constants/config.ts
@@ -1,4 +1,5 @@
+const BE_DEV_PATH = 'https://pals-app-b3zs.onrender.com';
 const BE_PROD_PATH = 'http://72.61.229.235';
 const BE_LOCAL_PATH = 'http://localhost:8080';
 
-export const ACTIVE_BE = BE_PROD_PATH;
+export const ACTIVE_BE = BE_DEV_PATH;
