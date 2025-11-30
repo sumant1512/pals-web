a6ba7a1f7425b0630b4ad7d98f2b0c5dd6e587aa
sumant1512 <sumantmishra511@gmail.com>
2025-11-30 13:47:35 +0530

    updating backend path

--- src/app/shared/constants/config.ts
+++ src/app/shared/constants/config.ts
@@ -2,4 +2,4 @@ const BE_DEV_PATH = 'https://pals-app-b3zs.onrender.com';
 const BE_PROD_PATH = 'https://api.palspaint.com';
 const BE_LOCAL_PATH = 'http://localhost:8080';
 
-export const ACTIVE_BE = BE_LOCAL_PATH;
+export const ACTIVE_BE = BE_PROD_PATH;
