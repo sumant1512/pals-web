602f5322220329a08ee8ca09e67b55e447050850
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2025-12-10 17:15:01 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../shared/services/authentication.service'
 
 @Component({
@@ -6,9 +6,13 @@ import { AuthenticationService } from '../shared/services/authentication.service
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   isBeActive = false;
   constructor(private readonly authenticationService: AuthenticationService){}
+
+  ngOnInit():void {
+    this.fetchBeHealth();
+  }
 
   fetchBeHealth():void{
     this.authenticationService.isBeActive().subscribe(resp => {
