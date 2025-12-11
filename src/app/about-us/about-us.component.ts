5972c7fd6bc0cfd98ec84c61de20a167870b114b
sumant1512 <sumantmishra511@gmail.com>
2025-12-11 23:17:13 +0530

    updated images to webp

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -11,19 +11,19 @@ import { Component } from '@angular/core';
 export class AboutUsComponent {
   palsCapabilities = [
     {
-      sequence: `${window.location.origin}/one.png`,
+      sequence: `${window.location.origin}/one.webp`,
       heading: 'Our Strengths',
       description:
         'Pals Paint manufactures and markets a wide range of decorative paints for homes, offices and commercial spaces. Its industrial and performance coatings cater to various sectors, including automotive, infrastructure, engineering, consumer goods and construction, providing reliable and durable surface-protection solutions.',
     },
     {
-      sequence: `${window.location.origin}/two.png`,
+      sequence: `${window.location.origin}/two.webp`,
       heading: 'Our Capabilities',
       description:
         'The company’s chemicals and specialty products division supports industries such as polymers, composites, packaging and manufacturing. With a strong focus on R&D, Pals Paint introduces new technologies and formulations each year to strengthen its position as an innovative specialty materials and coatings company.',
     },
     {
-      sequence: `${window.location.origin}/three.png`,
+      sequence: `${window.location.origin}/three.webp`,
       heading: 'Our Expertise',
       description:
         'Pals Paint operates modern, environmentally compliant manufacturing facilities supported by a skilled workforce and strong distribution network. The company prioritizes Health, Safety, Environment & Sustainability to protect its people and community.',
